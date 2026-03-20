36d4070bc37798ddc820e7f47ff2100c8badfe87
rlarjsdud110 <rlarjsdud110@naver.com>
2026-03-20 18:45:54 +0900

    Initial commit

---
+++ src/main/java/org/portfolio/pinpoint/PinPointApplication.java
@@ -0,0 +1,13 @@
+package org.portfolio.pinpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/portfolio/pinpoint/PinPointApplicationTests.java
@@ -0,0 +1,13 @@
+package org.portfolio.pinpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
